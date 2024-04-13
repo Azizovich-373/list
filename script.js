363709c72bdfeed8310ce3fec6962133a9436bc2
SAMIR GANIYEV <samirganiev777@gmail.com>
2024-04-13 12:41:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let arr = ['damir', 'samir', 'farzod', 'shoxruh']
+let guestName = prompt('Write any, name pls')
+if (arr = arr.filter(item => item !== guestName)) {
+    alert('The name was removed')
+} else {
+    alert('Name is ubdefuded')
+}
+
+
+console.log(arr);
